6a6e50906cd470c8630bf4efd2ce3fe89610b277
Qiaozhi <ql316@cam.ac.uk>
2023-02-10 19:22:55 +0000

    Moved enemy prefab

---
+++ Assets/_Scripts/EnemyHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : Health
+{
+    protected override void Die()
+    {
+        Debug.Log($"{gameObject.name} has died.");
+    }
+}
