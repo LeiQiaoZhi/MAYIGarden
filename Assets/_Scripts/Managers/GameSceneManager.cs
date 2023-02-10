0199f03bdb615e4103b12163a9ec00e585205a6b
Qiaozhi <ql316@cam.ac.uk>
2023-02-10 23:00:06 +0000

    Fix timescale in scene manager

--- Assets/_Scripts/Managers/GameSceneManager.cs
+++ Assets/_Scripts/Managers/GameSceneManager.cs
@@ -9,16 +9,19 @@ public class GameSceneManager : MonoBehaviour
     {
         Debug.Log("reload scene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 
     public void LoadScene(int index)
     {
         Debug.Log($"Loading scene {SceneManager.GetSceneByBuildIndex(index).name}");
         SceneManager.LoadScene(index, LoadSceneMode.Single);
+        Time.timeScale = 1;
     }
 
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1;
     }
 }
