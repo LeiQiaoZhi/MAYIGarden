c8bdfa3461185a8d8d6e1b15a22ceabe2c4869bd
Qiaozhi <ql316@cam.ac.uk>
2023-02-10 21:50:19 +0000

    Enemy Attack and small change to GameManager.cs

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float stopThreshold = 0.2f;
+    
+    public float secondsBetweenAttacks = 1f;
+    public int attackDamage = 1;
+    
+    private AIDestinationSetter _destinationSetter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _destinationSetter = GetComponent<AIDestinationSetter>();
+    }
+
+    // Update is called once per frame
+    private bool _attacking;
+    void Update()
+    {
+        if (_destinationSetter.target == null)
+        {
+            return;
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!_attacking && col.CompareTag("Root"))
+        {
+            Debug.LogWarning("Detect Root");
+            // stop and attack
+            _attacking = true;
+            StartCoroutine(AttackCoroutine(_destinationSetter.target));
+            _destinationSetter.enabled = false; 
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (_attacking && other.CompareTag("Root"))
+        {
+            _attacking = false;
+            _destinationSetter.enabled = true;
+        }
+    }
+
+    IEnumerator AttackCoroutine(Transform target)
+    {
+        while (_attacking)
+        {
+            Attack(target);
+            yield return new WaitForSeconds(secondsBetweenAttacks);
+        }
+    }
+
+
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    void Attack(Transform target)
+    {
+        Debug.Log($"Attacking {target.name}");
+        Health health = target.GetComponent<Health>();
+        if (health == null)
+        {
+            Debug.LogError($"{target.name} doesn't have Health");
+        }
+        else
+        {
+            health.ChangeHealth(-attackDamage);
+        }
+    }
+}
--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -12,8 +12,15 @@ public class GameManager : MonoBehaviour
     [System.Serializable]
     public class Wave
     {
-        
-        
+        [System.Serializable]
+        public class EnemyInfo
+        {
+            public GameObject enemyPrefab;
+            public int enemyNumber;
+        }
+
+        public List<EnemyInfo> enemies;
+        public float averageSecondsBetweenSpawns;
     }
     
     public GameEvent gameOverEvent;
